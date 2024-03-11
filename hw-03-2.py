6fb0b2da8b60dc2d4c4c19b4f46e307a5560ee53
MadeInUA31616 <vladilechko31616@gmail.com>
2024-03-12 01:39:44 +0200

    changed

--- hw-03-2.py
+++ hw-03-2.py
@@ -1,10 +1,12 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
+    if min < 1 or max > 1000 or quantity < 1 or quantity > (max - min + 1):
+        return []
+    
     for _ in range(quantity):
-        numbers = list(random.sample(range(min, max), 5))
-
-    return numbers
+        numbers = list(random.sample(range(min, max), quantity))
+    return sorted(numbers)
 
 result = get_numbers_ticket(1, 31, 5)
 print(f"Ваші лотерейні числа: {result}")
